d4ce4ea4912effd942acf7d8183691134d9b1c07
Oomija Taku <n18005@std.it-college.ac.jp>
2018-05-18 06:33:48 +0000

    index.html

--- test.sh
+++ test.sh
@@ -4,7 +4,13 @@ today=$(date'+%d')
 
 #echo $today
 
-if[$today -eq 20]; then
- echo "1"
+if [$today -eq 20]; then
+ echo "今日は20日です。月次〆処理を開始してください"
 elif[$today -lt 20]; then
- echo"2"
+echo"今日は20日より前の日にちです。"
+elif[$today -lt 20]; then
+echo"今日は20日より後の日にちです。"
+ elif[$today -lt 20]; then
+else
+echo"err:日付が取れませんでした。"
+fi
